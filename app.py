f99d282e5d2925a78883546beca4b7cdc9598c55
Nilashan Arul <71449959+hattrickjjmo@users.noreply.github.com>
2024-04-12 13:52:14 +0200

    Fixed chatbot interface for chatbot

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import os
 import gradio as gr
 from botfunctions import create_database
+import time
 
 # Preamble
 os.environ["OPENAI_API_KEY"] = "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"
@@ -9,15 +10,13 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY")
 )
 
-
-
 # Defining the role of the AI
 messages = [
     {"role": "system", "content": "You are a helpful and kind AI Assistant who specializes in immigration to Norway. You answer only to questions regarding how people from different countries can immigrate to Norway, in other words give answers about each of the steps on has to take and how to take them. You may also answer questions regarding steps one can take after arriving in Norway, such as how to apply for loans, or find housing and such."},
 ]
 
-# Main function for chatbot prototype
-def chatbot(input):
+# Main function for receiving chat completion (reply) from ChatGPT
+def completion(input, history):
     if input:
         messages.append({"role": "user", "content": input})
         chat = client.chat.completions.create(
@@ -25,12 +24,30 @@ def chatbot(input):
         )
         reply = chat.choices[0].message.content
         messages.append({"role": "assistant", "content": reply})
-        return reply
+        # Faking streaming chat just for visuals
+        for i in range(len(reply)):
+            time.sleep(0.003)
+            yield reply[:i+i]
 
-inputs = gr.components.Textbox(lines=7, label="Chat with AI")
-outputs = gr.components.Textbox(label="Reply")
+# Gradio's own premade chat interface
+gr.ChatInterface(
+    completion, 
+    chatbot=gr.Chatbot(height=500), 
+    textbox=gr.Textbox(placeholder="Ask me any questions about immigrating to Norway", container=False, scale=7, height=100), 
+    title="ImmiBot", 
+    description="ImmiBot answers anything you may wonder about regarding immigration to Norway",
+    theme="soft",
+    #examples=["I am from Denmark. How do I begin my journey to immigrate to Norway?", "What is BankID", "Can you conversate in Spanish?"],
+    #cache_examples=True,
+    retry_btn=None, 
+    undo_btn=None
+    ).launch(share=True)
+
+
+
+# These are from the previous solution
+# inputs = gr.components.Textbox(lines=7, label="Chat with AI")
+# outputs = gr.components.Textbox(label="Reply")
 
 # create_database()
-gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="ImmiBot", description="Ask me anything you want about the immigration process to Norway :)", theme="compact").launch(share=True)
-
-
+# gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="ImmiBot", description="Ask me anything you want about the immigration process to Norway :)", theme="compact").launch(share=True)
